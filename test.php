28f0a73a05d5a634ce9123f7d58f2f4be281c417
RutsuKun <henicz19@gmail.com>
2021-04-20 16:14:22 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,24 @@
 
+<?php
+ 
+namespace Tests\Test;
+ 
+use Tests\TestCase;
+ 
+class Test extends TestCase
+{
+
+ 
+    /**
+     * @feature Test
+     * @scenario Test Scenario
+     * @case Get Test
+     * @suite: Get Test
+     * @test
+     */
+    public function test()
+    {
+	        //TODO implement
+    }
+
+    }
