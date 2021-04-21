c679b8c10fdb56e731f3d22b041dd868e3b09478
RutsuKun <henicz19@gmail.com>
2021-04-21 14:09:35 +0200

    testing.php

---
+++ testing.php
@@ -0,0 +1,14 @@
+class Testing
+{
+    /**
+     * @feature Testing
+     * @scenario Testing Scenario
+     * @case Get Testing
+     * @suite: Get Testing
+     * @test
+     */
+    public function testing()
+    {
+	        //TODO implement
+    }
+ }
