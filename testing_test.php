01b50c4d2cdd9b1a18fc45a41a1dcf48f56f298e
RutsuKun <henicz19@gmail.com>
2021-05-21 10:45:27 +0200

    Create testing_test.php

---
+++ testing_test.php
@@ -0,0 +1,15 @@
+class TestingTest
+{
+    /**
+     * @feature Testing Test
+     * @scenario Testing Test Scenario 1
+     * @case Get Testing Test 1
+     * @suite: Get Testing Test 1
+     * @test
+     */
+    public function testing_test_one()
+    {
+	        //TODO implement
+    }
+    
+}
