3be215ea320f28e573734495007bd02d1729e018
RutsuKun <mateusz.h@devpark.pl>
2021-06-07 12:19:01 +0000

    RutsuKun!!-!!mateusz.h@devpark.pl

--- testing_two.php
+++ testing_two.php
@@ -13,4 +13,34 @@ class TestingTwo
 	        //TODO implement
     }
     
+
+    /**
+     * @feature Enter
+     * @scenario Enter Click
+     * @case Case 1
+     *
+     * @description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend nec ante sit
+     * amet sodales. Praesent in scelerisque urna. Praesent sit amet nisi nisi.
+     * Morbi sodales quis felis in pretium. Nam scelerisque, dolor eleifend mattis
+     * imperdiet, ipsum nisl aliquam quam, at dignissim nulla risus ut sem.
+     * Proin aliquet quis erat vel pulvinar. Quisque sodales diam et ultricies
+     * porta. Sed pharetra varius pulvinar. Vestibulum vulputate mollis libero,
+     * id tincidunt turpis volutpat vitae.
+
+Sed ac mauris tempus, tristique risus
+     * eget, feugiat nibh. Donec faucibus nulla ac magna varius consectetur.
+     * Curabitur id eros at dolor dignissim posuere vitae et dolor. Donec vehicula
+     * quis massa quis placerat. Maecenas at urna vitae est viverra vestibulum
+     * interdum id odio. Ut laoreet vehicula justo a porta. Suspendisse venenatis
+     * erat justo, vitae imperdiet lacus tincidunt volutpat. Curabitur sollicitudin
+     * feugiat sem, ut tempor leo feugiat vitae. Morbi cursus sagittis purus,
+     * eu gravida elit aliquam at. Curabitur laoreet vehicula justo, sed vulputate
+     * est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+     * posuere cubilia curae; Fusce malesuada sodales lacus sit amet tristique.
+     * @test
+     */
+    public function teeeeeeeeeeeeeeeeeeeeeeeestts()
+    {
+        //TODO implement
+    }
 }
