775f8cbfad930d490ca0918cab4671c4b5b241ec
RutsuKun <henicz19@gmail.com>
2021-08-12 12:30:51 +0200

    Update nowyDoTestowania.php

--- nowyDoTestowania.php
+++ nowyDoTestowania.php
@@ -1,3 +1,22 @@
 <?php
+namespace Tests\Test;
+ 
+use Tests\TestCase;
+ 
+class Testwgrwgwgrwgrwg extends TestCase
+{
 
+ 
+    /**
+     * @feature wrwgrwgrwh
+     * @scenario wrhwhrwhrwhrrwh
+     * @case wrhwhwhwh
+     * @suite: wrhwhrwhrwhrwh
+     * @test
+     */
+    public function test()
+    {
+	        //TODO implement
+    }
+}
 ?>
