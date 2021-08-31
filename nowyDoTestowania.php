28e78b5b83e32358743aaaffed9b6945caa35d4c
RutsuKun <henicz19@gmail.com>
2021-08-31 14:25:15 +0200

    Update nowyDoTestowania.php

--- nowyDoTestowania.php
+++ nowyDoTestowania.php
@@ -3,7 +3,7 @@ namespace Tests\Test;
  
 use Tests\TestCase;
  
-class Testwgrwgwgrwgrwg extends TestCase
+class SuperTestsWithDataProviders extends TestCase
 {
 
  
@@ -12,11 +12,23 @@ class Testwgrwgwgrwgrwg extends TestCase
      * @scenario wrhwhrwhrwhrrwh
      * @case wrhwhwhwh
      * @suite: wrhwhrwhrwhrwh
+     *
      * @test
+     * @dataProvider testDataProvider
      */
-    public function test()
+    public function superTestWwithDataProvider(String $one, Integer $two)
     {
-	        //TODO implement
+      // TODO implement
+    }
+	
+    public function testDataProvider()
+    {
+        return [
+            'test 1' => [ 'one' => 'ONE', 'two' => 1],
+            'test 2' => [ 'one' => 'THREE', 'two' => 3],
+            'test 3' => [ 'one' => 'THREE', 'two' => 3],
+            'test 4' => [ 'one' => 'SEVEN', 'two' => 7]
+        ];
     }
 }
 ?>
