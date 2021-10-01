f0e2f9534f10d634d572a66029d2d0458398ac3d
RutsuKun <user@user.com>
2021-10-01 10:41:49 +0000

    RutsuKun-user@user.com at: 2021-10-01 10:41:49 UTC

--- testing_two.php
+++ testing_two.php
@@ -14,30 +14,10 @@ class TestingTwo
     }
 
     /**
-     * @feature Enterekkkkk
-     * @scenario Enter Clickedkkk
-     * @case Case 1 editedkkk
-     *
      * @suite new suite
-     * @description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend nec ante sit
-     * amet sodales. Praesent in scelerisque urna. Praesent sit amet nisi nisi.
-     * Morbi sodales quis felis in pretium. Nam scelerisque, dolor eleifend mattis
-     * imperdiet, ipsum nisl aliquam quam, at dignissim nulla risus ut sem.
-     * Proin aliquet quis erat vel pulvinar. Quisque sodales diam et ultricies
-     * porta. Sed pharetra varius pulvinar. Vestibulum vulputate mollis libero,
-     * id tincidunt turpis volutpat vitae. Sed ac mauris tempus, tristique risus
-     * eget, feugiat nibh. Donec faucibus nulla ac magna varius consectetur. Curabitur
-     * id eros at dolor dignissim posuere vitae et dolor. Donec vehicula
-     * quis massa quis placerat. Maecenas at urna vitae est viverra vestibulum
-     * interdum id odio. Ut laoreet vehicula justo a porta. Suspendisse venenatis
-     * erat justo, vitae imperdiet lacus tincidunt volutpat. Curabitur sollicitudin
-     * feugiat sem, ut tempor leo feugiat vitae. Morbi cursus sagittis purus,
-     * eu gravida elit aliquam at. Curabitur laoreet vehicula justo, sed vulputate
-     * est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-     * posuere cubilia curae; Fusce malesuada sodales lacus sit amet tristique.
      * @test
      */
-    public function teeeeeeeeeeeeeeeeeeeeeeeestts()
+    public function enter_teser
     {
         //TODO implement
     }
