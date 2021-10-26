29550e301ebe8d161a1f8b4dfa7681fdf47d726a
RutsuKun <mateusz.h@devpark.pl>
2021-10-26 14:08:09 +0000

    RutsuKun-mateusz.h@devpark.pl at: 2021-10-26 02:08:09 UTC

--- nowyDoTestowania.php
+++ nowyDoTestowania.php
@@ -53,7 +53,7 @@ class SuperTestsWithDataProviders extends TestCase
     public function testDataProviderSecond()
     {
         return [
-            'test 5' => ['one' => 'ONE', 'two' => 1],
+            'test 555555' => ['one' => 'ONEwrgrwgwgw', 'two' => 1],
             'test 6' => ['one' => 'THREE', 'two' => 3],
             'test 7' => ['one' => 'THREE', 'two' => 3],
             'test 8' => ['one' => 'SEVEN', 'two' => 7],
