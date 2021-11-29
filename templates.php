d2923fd34fbcec5c16f5ff7a7a028ece9c8b53f5
RutsuKun <mateusz.h@devpark.pl>
2021-11-29 11:33:25 +0000

    RutsuKun-mateusz.h@devpark.pl at: 2021-11-29 11:33:25 UTC

---
+++ templates.php
@@ -0,0 +1,24 @@
+{
+
+    /**
+     * @feature 111
+     * @scenario 222
+     * @case 333
+     *
+     * @expectation none
+     *
+     * @description szablon ktory i tak nic nie robi
+     *
+     * @test
+     */
+    public function _1638185605_mateusz_h_AT_devpark_pl()
+    {
+        //TODO UniTree template
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+    }
+}
